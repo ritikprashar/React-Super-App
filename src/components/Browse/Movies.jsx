d43e2a6c9677ff0338e4494919b94f9ff7e78c86
ritikprashar <prasharritik12@gmail.com>
2023-10-23 18:09:36 +0530

    add movies api

---
+++ src/components/Browse/Movies.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import styles from "./Movies.module.css";
+const Movies = ({ type }) => {
+  const [movies, setMovies] = useState([]);
+  console.log(movies);
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "84d716def9mshfda12e4c205103ep172fcejsncd20c7a2ef26",
+        "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+      },
+    };
+    const fetchMovies = async () => {
+      await fetch(
+        `https://moviesdatabase.p.rapidapi.com/titles?genre=${type}&year=2020`,
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => setMovies(response.results.splice(4, 4)))
+        .catch((err) => console.error(err));
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      <p className={styles.heading} style={{ overflowY: "hidden" }}>
+        {type}
+      </p>
+      <div style={{ display: "flex", overflow: "hidden", marginLeft: "2vw" }}>
+        {movies.map((movie, idx) => {
+          return (
+            <div
+              key={idx}
+              style={{
+                width: "20vw",
+                margin: "2vw",
+              }}
+            >
+              <img
+                src={movie?.primaryImage?.url}
+                alt=""
+                style={{
+                  objectFit: "cover",
+                  width: "18vw",
+                  height: "33vh",
+                  borderRadius: "12px",
+                }}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Movies;
--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,27 +1,29 @@
 import Movies from "../components/Browse/Movies";
 import ProfilePic from "../assets/dpSmall.png";
 const Browse = () => {
+  const browse = JSON.parse(window.localStorage.getItem("type"));
   return (
     <>
-      <img
-        src={ProfilePic}
-        style={{
-          position: "absolute",
-          top: "6vh",
-          right: "4vw",
-          height: "60px",
-          width: "60px",
-        }}
-        alt=""
-      />
       <div
         style={{
           width: "100vw",
           minHeight: "100vh",
           background: "black",
-          overflowX: "hidden",
+          overflowY: "scroll",
+          maxHeight: "100vh",
         }}
       >
+        <img
+          src={ProfilePic}
+          style={{
+            position: "absolute",
+            top: "6vh",
+            right: "4vw",
+            height: "60px",
+            width: "60px",
+          }}
+          alt=""
+        />
         <p
           style={{
             fontSize: "2.3rem",
@@ -35,7 +37,7 @@ const Browse = () => {
         </p>
         <p
           style={{
-            fontSize: "1.8rem",
+            fontSize: "1.6rem",
             margin: "2vw 4vw",
             color: "#FFF",
             fontFamily: "Roboto",
@@ -45,9 +47,10 @@ const Browse = () => {
         >
           Entertainment according to your choice
         </p>
-
+        {browse.map((movie) => (
+          <Movies type={movie} />
+        ))}
       </div>
-      <Movies />
     </>
   );
 };
